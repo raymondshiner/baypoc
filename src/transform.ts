a5a9f9f641440c74f9e356fffd76dbff9e3bd609
Raymond Shiner <rshiner@Raymond-Shiner.local>
2022-05-03 16:06:15 -0700

    image optimization

--- src/transform.ts
+++ src/transform.ts
@@ -19,6 +19,43 @@ export default function transform(response: Response, request: Request) {
       <script src="/main.js" defer="defer"></script>
     `)
 
+    const prioritizeImage = (el: any, src: string = $(el).attr('src') || '') => {
+      $(el).attr('loading', 'eager').attr('fetchpriority', 'high')
+      if (src && src.length > 0 && src !== 'undefined') {
+        preloadImage(src)
+      }
+      // el: Provide <img> element you want prioritized
+      // src (optional): Image url. By default it uses <img> `src` attribute.
+    }
+
+    const preloadImage = (src: string) => {
+      $('head').prepend(`<link rel="preload" class="l0-preload" as="image" href="${src}">`)
+    }
+
+    const preloadCss = (src: string) => {
+      $('head').prepend(`<link rel="preload" class="l0-preload" as="style" href="${src}">`)
+    }
+
+    const preloadFont = (src: string) => {
+      $('head').prepend(`<link rel="preload" class="l0-preload" as="font" href="${src}">`)
+    }
+
+    preloadCss('/on/demandware.static/Sites-TheBay-Site/-/en_CA/v1649831552895/css/global.css')
+
+    // Home
+    if (request.path == '/') {
+      $('body').prepend('<a href="/c/women/womens-clothing"/>')
+      prioritizeImage('.content-body.withinRight .content-image img')
+    }
+
+    // Lazy load all images and run through moovweb optimizer //
+    $('img').each((i, el) => {
+      $(el)
+        .attr('loading', 'lazy')
+        .attr('fetchpriority', 'low')
+        .attr('src', `https://opt.moovweb.net?img=${el.attribs.src}`)
+    })
+
     // uncomment this line to enable replacePersonalizedData
     // replacePersonalizedData($)
 
